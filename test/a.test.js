69b7ace23c453432c4834ebb87628478c6df6937
Eric Prud'hommeaux <eric+github@w3.org>
2020-05-05 16:15:35 +0200

    sequential

--- test/a.test.js
+++ test/a.test.js
@@ -3,11 +3,7 @@ const chaiAsPromised = require('chai-as-promised')
 const expect = chai.expect
 chai.use(chaiAsPromised)
 const H = require('./shared')
-const p = H.init({a:1})
-p.then(
-  i =>
-    console.log(i, 'a initialized', H)
-)
+const p = H.init({a:1}, i => console.log(i, 'a initialized', H))
 
 describe('a', () => {
   it('should let 1 be 1', () => {
--- test/shared.js
+++ test/shared.js
@@ -1,9 +1,19 @@
+const start = Date.now()
 
 const ret = {
   init,
+  wait: function (label, time, ret) {
+    console.log(`start timer ${label} ${time} ${ret}`, Date.now()-start)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(`end timer ${label} ${time} ${ret}`, Date.now()-start)
+        resolve(ret)
+      }, time)
+    })
+  },
 }
 
-function init (merge) {
+function init (merge, f) {
   init.initialized = init.initialized || new Promise((resolve, reject) => {
     console.log('setTimeout');
     setTimeout(() => resolve(0), 1000)
@@ -12,7 +22,7 @@ function init (merge) {
     Object.assign(ret, merge);
     return i+1
   }))
-  return init.initialized
+  return init.initialized.then(f)
 }
 
 module.exports = ret;
